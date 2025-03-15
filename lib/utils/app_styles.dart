db1e20e25b31e4344a124d31fe3a8c4a9c256eb3
Khalifa <Khalifa@DESKTOP-EIURBPD.com>
2025-03-15 15:50:50 +0200

    Project initialized

---
+++ lib/utils/app_styles.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/widgets.dart';
+
+abstract class AppStyles {
+  static const TextStyle regular16 = TextStyle(
+    color: Color(0xFF064061),
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Montserrat',
+  );
+  static const TextStyle regular12 = TextStyle(
+    color: Color(0xFFAAAAAA),
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Montserrat',
+  );
+  static const TextStyle regular14 = TextStyle(
+    color: Color(0xFFAAAAAA),
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Montserrat',
+  );
+  static const TextStyle mediam16 = TextStyle(
+    color: Color(0xFF064061),
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'Montserrat',
+  );
+  static const TextStyle mediam20 = TextStyle(
+    color: Color(0xFFFFFFFF),
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'Montserrat',
+  );
+  static const TextStyle semiBold16 = TextStyle(
+    color: Color(0xFF064061),
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Montserrat',
+  );
+  static const TextStyle semiBold18 = TextStyle(
+    color: Color(0xFFFFFFFF),
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Montserrat',
+  );
+  static const TextStyle semiBold20 = TextStyle(
+    color: Color(0xFF064061),
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Montserrat',
+  );
+  static const TextStyle semiBold24 = TextStyle(
+    color: Color(0xFF4EB7F2),
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Montserrat',
+  );
+  static const TextStyle bold16 = TextStyle(
+    color: Color(0xFF4EB7F2),
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'Montserrat',
+  );
+}
---
+++ lib/utils/assets_manager.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: prefer_single_quotes
+class AssetsManager {
+  AssetsManager._();
+  
+  static const String imagesAvatar1 = "assets/images/avatar1.svg";
+
+  static const String imagesAvatar2 = "assets/images/avatar2.svg";
+
+  static const String imagesBalance = "assets/images/balance.svg";
+
+  static const String imagesCardShape = "assets/images/card_shape.svg";
+
+  static const String imagesDashboard = "assets/images/dashboard.svg";
+
+  static const String imagesExpenses = "assets/images/expenses.svg";
+
+  static const String imagesGallery = "assets/images/gallery.svg";
+
+  static const String imagesIncome = "assets/images/income.svg";
+
+  static const String imagesLogout = "assets/images/logout.svg";
+
+  static const String imagesMyInvestment = "assets/images/my_investment.svg";
+
+  static const String imagesMyTransaction = "assets/images/my_transaction.svg";
+
+  static const String imagesSetting = "assets/images/setting.svg";
+
+  static const String imagesStatistics = "assets/images/statistics.svg";
+
+  static const String imagesWelletAcount = "assets/images/wellet_acount.svg";
+}
+
